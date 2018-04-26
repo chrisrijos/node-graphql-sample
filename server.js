0792f968ad237d0ee4462bd45bd320cd64fb48df
crijos <crijos@reixcorp.com>
2018-04-26 13:41:25 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express'),
+      express_graphql = require('express-graphql')
+      app = express();
+
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+    type Query {
+      message: String
+    }
+`);
+
+var root = {
+  message: () => 'Hello World!'
+};
+
+app.use('/graphql', express_graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+app.listen(4000, () => { console.log('App listening on 4000')});
---
+++ server2.js
@@ -0,0 +1,77 @@
+const express = require('express'),
+      express_graphql = require('express-graphql'),
+      app = express();
+
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    course(id: Int!): Course
+    courses(topic: String): [Course]
+  },
+  type Course {
+    id: Int
+    title: String
+    author: String
+    description: String
+    topic: String
+    url: String
+  }
+`);
+
+//static data for testing
+var coursesData = [ 
+  {
+      id: 1,
+      title: 'The Complete Node.js Developer Course',
+      author: 'Andrew Mead, Rob Percival',
+      description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+      topic: 'Node.js',
+      url: 'https://codingthesmartway.com/courses/nodejs/'
+  },
+  {
+      id: 2,
+      title: 'Node.js, Express & MongoDB Dev to Deployment',
+      author: 'Brad Traversy',
+      description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+      topic: 'Node.js',
+      url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
+  },
+  {
+      id: 3,
+      title: 'JavaScript: Understanding The Weird Parts',
+      author: 'Anthony Alicea',
+      description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+      topic: 'JavaScript',
+      url: 'https://codingthesmartway.com/courses/understand-javascript/'
+  }
+]
+
+var getCourse = (args) => {
+  var id = args.id;
+  return coursesData.filter(course => {
+    return course.id == id;
+  })[0];
+}
+
+var getCourses = function(args) {
+  if (args.topic) {
+      var topic = args.topic;
+      return coursesData.filter(course => course.topic === topic);
+  } else {
+      return coursesData;
+  }
+}
+
+var root = {
+  course: getCourse,
+  courses: getCourses
+};
+
+app.use('/graphql', express_graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+app.listen(4000, () => { console.log('App listening on 4000')});
